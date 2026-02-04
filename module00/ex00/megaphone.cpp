8fea68f7292884c5b0944482ec3205cc49ec3635
Mahmut Kilic <mahkilic@f1r3s7.codam.nl>
2026-02-04 15:05:38 +0100

    little update and new syntax on module00/ex00

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,30 +6,24 @@
 /*   By: mahkilic <mahkilic@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/01/30 22:31:15 by mahkilic      #+#    #+#                 */
-/*   Updated: 2026/01/30 22:31:15 by mahkilic      ########   odam.nl         */
+/*   Updated: 2026/02/04 14:54:04 by mahkilic      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int		main(int argc, char **argv)
+int	main(int ac, char **av)
 {
-	if (argc > 1)
+	if (ac < 2)
 	{
-		int i = 1;
-		while (i < argc)
-		{
-			int j = 0;
-			while (argv[i][j])
-			{
-				std::cout << (char)toupper(argv[i][j]);
-				j++;
-			}
-			i++;
-		}
-		std::cout << std::endl;
-	}
-	else
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)std::toupper(av[i][j]);
+	}
+	std::cout << std::endl;
 	return (0);
 }
