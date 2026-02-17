dd5070a1073df737d7eb3ba74b8214e4c42c9845
Mahmut Kilic <mahmutsedatkilic@gmail.com>
2026-02-18 00:57:51 +0100

    module05/ex00

---
+++ module05/ex00/include/Bureaucrat.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Bureaucrat.hpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mahkilic <mahkilic@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/02/18 00:34:19 by mahkilic      #+#    #+#                 */
+/*   Updated: 2026/02/18 00:34:19 by mahkilic      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_H
+# define BUREAUCRAT_H
+
+# include <string>
+# include <iostream>
+# include <exception>
+
+class Bureaucrat
+{
+private:
+	const std::string	_name;
+	int					_grade;
+public:
+	Bureaucrat(void);
+	Bureaucrat(const std::string &name, int grade);
+	Bureaucrat(const Bureaucrat &other);
+	Bureaucrat &operator=(const Bureaucrat &other);
+	~Bureaucrat(void);
+	
+	const std::string	&getName(void) const;
+	int					getGrade(void) const;
+	void					incrementGrade(void);
+	void					decrementGrade(void);
+
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char	*what(void) const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char	*what(void) const throw();
+	};
+};
+
+std::ostream	&operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+#endif
---
+++ module05/ex00/src/main.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mahkilic <mahkilic@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/02/18 00:34:06 by mahkilic      #+#    #+#                 */
+/*   Updated: 2026/02/18 00:34:06 by mahkilic      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Bureaucrat.hpp"
+
+int	main(void)
+{
+	std::cout << "=== Valid case ===" << std::endl;
+	try
+	{
+		Bureaucrat	a("Alice", 2);
+		std::cout << a << std::endl;
+		a.incrementGrade();
+		std::cout << a << std::endl;
+		a.incrementGrade();
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	std::cout << std::endl << "=== Low bound case ===" << std::endl;
+	try
+	{
+		Bureaucrat	b("Bob", 149);
+		std::cout << b << std::endl;
+		b.decrementGrade();
+		std::cout << b << std::endl;
+		b.decrementGrade();
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	std::cout << std::endl << "=== Invalid constructor case ===" << std::endl;
+	try
+	{
+		Bureaucrat	c("TooHigh", 0);
+		std::cout << c << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception for grade 0: " << e.what() << std::endl;
+	}
+
+	try
+	{
+		Bureaucrat	d("TooLow", 151);
+		std::cout << d << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception for grade 151: " << e.what() << std::endl;
+	}
+	return (0);
+}
